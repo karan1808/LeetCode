e0fca29b22554c1726615de4d8c2b483edad0caf
karan1808 <karanlamba1808@gmail.com>
2020-07-24 14:58:13 +0530

    24/7/20

---
+++ 454. 4Sum II.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int, int> absum;
+
+        for (auto a : A) {
+            for (auto b : B) {
+                absum[a + b]++;
+            }
+        }
+
+
+        int res = 0;
+
+        for (auto c : C) {
+            for (auto d : D) {
+                auto it = absum.find(0 - c - d);
+                if (it != absum.end()) {
+                    res += it->second;
+                }
+            }
+        }
+        return res;
+    }
+};
